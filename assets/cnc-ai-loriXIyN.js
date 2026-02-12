9443ea6d56df5cf41f305c00c2ae11c95578340d
clbenoit <clbenoit@users.noreply.github.com>
2026-02-12 10:28:27 +0000

    deploy: e4daa875b2cb5d5ad6aa71f0b4729467a5f152d2

--- assets/cnc-ai-loriXIyN.js
+++ assets/cnc-ai-loriXIyN.js
@@ -1,4 +1,4 @@
-import{u as r,j as e}from"./index-5-TB8A-S.js";const a={layout:"default",title:"CNC AI Models",description:"undefined"};function i(t){const n={a:"a",div:"div",em:"em",h1:"h1",header:"header",li:"li",p:"p",strong:"strong",ul:"ul",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"cnc-ai-models",children:["CNC AI Models",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#cnc-ai-models",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+import{u as r,j as e}from"./index-C7fxtPC8.js";const a={layout:"default",title:"CNC AI Models",description:"undefined"};function i(t){const n={a:"a",div:"div",em:"em",h1:"h1",header:"header",li:"li",p:"p",strong:"strong",ul:"ul",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"cnc-ai-models",children:["CNC AI Models",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#cnc-ai-models",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
 `,e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,playsInline:!0,disablePictureInPicture:!0,disableRemotePlayback:!0,style:{maxWidth:"100%",height:"auto"},children:e.jsx("source",{src:"/portfolio/ai.webm",type:"video/webm"})}),`
 `,e.jsxs(n.p,{children:["Accessible AI models hosted on a dedicated platform for ",e.jsx(n.em,{children:"CNC crew members"})]}),`
 `,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://ai.omicsverse.fr",children:"Access the platform CNC AI platform"})}),`
@@ -14,4 +14,5 @@ import{u as r,j as e}from"./index-5-TB8A-S.js";const a={layout:"default",title:"
 `,e.jsx(n.li,{children:"Advanced Data Science (Inferential statistics & Graph Theory)"}),`
 `,e.jsx(n.li,{children:"Blockchain-enabled AI (On-chain data analysis & Smart Contract interaction)"}),`
 `,e.jsx(n.li,{children:"High-uptime monitoring and distributed systems reliability"}),`
+`,e.jsxs(n.li,{children:["Integration with our Cloud services at ",e.jsx(n.a,{href:"https://cloud.omicsverse.fr",children:"cloud.omicsverse.fr"})]}),`
 `]})]})}function l(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{l as default,a as frontmatter};
